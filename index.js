03ed5a4ba4fe3d8f505577bff9f94122f2be5f49
hyejinxx <haejinjung1110@gmail.com>
2025-07-10 17:29:28 +1000

    fix: mongoDB connect check code edit

--- index.js
+++ index.js
@@ -17,13 +17,35 @@ app.use(bodyParser.json()); // parse JSON request bodies
 require('dotenv').config(); // load environment variables from .env file
 
 // connect to MongoDB
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
-  
-mongoose.connect(mongoURI).then(() => {
-    console.log('MongoDB 연결 성공');
-}).catch((error) => {
-    console.error('MongoDB 연결 실패:', error);
+const mongoURI = process.env.MONGODB_URI;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+  
+// mongoose.connect(mongoURI).then(() => {
+//     console.log('MongoDB 연결 성공');
+// }).catch((error) => {
+//     console.error('MongoDB 연결 실패:', error);
+// });
 
 
 app.get('/', (req, res) => {
